8d5da2f47b229b3d3fa66429b5f1071fea3c7572
Gstamelos <g.j.stamelos@gmail.com>
2022-05-23 23:14:55 +0300

    grab_mode_re-defining

--- src/main.cpp
+++ src/main.cpp
@@ -151,7 +151,7 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 16500000; //originally set to 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //config.grab_mode = CAMERA_GRAB_LATEST;
+  config.grab_mode = CAMERA_GRAB_LATEST;
 
 
 
